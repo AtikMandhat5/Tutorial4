e8be18314f101ec8ea1d58c5379632de6fd6010c
Atik <atikmandhat@gmail.com>
2021-08-27 21:43:49 +0530

    complete the SplashScreen & login tutorial no.4 ...

--- app/src/main/java/com/example/tutorial4/MainActivity.java
+++ app/src/main/java/com/example/tutorial4/MainActivity.java
@@ -2,19 +2,23 @@ package com.example.tutorial4;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
-    String username;
+
     TextView message;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        username=getIntent().getStringExtra(username);
+
+//        username=getIntent().getStringExtra(username);
         message=findViewById(R.id.message);
-        message.setText("WELCOME TO WORLD "+username);
+        message.setText("WELCOME TO WORLD ADMIN!");
+
     }
 }
--- app/src/main/java/com/example/tutorial4/SplashScreen.java
+++ app/src/main/java/com/example/tutorial4/SplashScreen.java
@@ -15,10 +15,12 @@ public class SplashScreen extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash_screen);
 
+        getSupportActionBar().hide();
+
         new Handler().postDelayed(new Runnable() {
             @Override
             public void run() {
-                Intent intent=new Intent(SplashScreen.this,MainActivity.class);
+                Intent intent=new Intent(SplashScreen.this,LoginActivity.class);
                 startActivity(intent);
                 finish();
 
